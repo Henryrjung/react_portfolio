1f927c92c80c25548618326260d05958fdcf96ce
HenryJ <henryjung667@gmail.com>
2021-02-10 14:33:53 -0600

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/home/Home';
+import Contact from './pages/contact/Contact';
+import Portfolio from './pages/portfolio/Portfolio';
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <div>
+      <Navbar />
+      <Route exact path='/' component={Home}/>
+      <Route path='/Contact' component={Contact} />
+      <Route path='/Portfolio' component={Portfolio} />
+    </div>
+    </Router>
+    
   );
 }
 
