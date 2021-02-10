14d1667de6afbad2a45943b1ba93c827e8d6cfc9
HenryJ <henryjung667@gmail.com>
2021-02-10 16:14:05 -0600

    added skills componenet

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Skills.css';
+
+function Skills() {
+  return (
+    <>
+    <div className='container'>
+        <div>
+        <h1>Technologies</h1>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+      
+    </>
+  );
+}
+
+export default Skills;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import About from '../../components/about/About';
+import Skills from '../../components/skills/Skills';
 
 function Home() {
   return (
     <>
      <About />
+     <Skills />
     </>
   );
 }
