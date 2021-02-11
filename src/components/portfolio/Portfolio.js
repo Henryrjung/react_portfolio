2f5fd0833d34117098cf9f0a67bdd497654386a1
HenryJ <henryjung667@gmail.com>
2021-02-11 15:22:52 -0600

    added card

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Portfolio.css';
+import Card from '../card/Card';
 
 function Projects(){
     return(
         <>
         <h1 className='logo'>Projects</h1>
+        <div className='container'>
+            <Card />
+        </div>
         </>
     )
 }
