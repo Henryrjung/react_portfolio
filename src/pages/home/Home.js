2164a0427717a01f11dbc505f94e19295a81fd5e
HenryJ <henryjung667@gmail.com>
2021-02-12 14:02:21 -0600

    imported sticky footer from material ui

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import About from '../../components/about/About';
 import Skills from '../../components/skills/Skills';
-
+import Footer from '../../components/footer/Footer';
 function Home() {
   return (
     <>
      <About />
      <Skills />
+      <Footer />
     </>
   );
 }
